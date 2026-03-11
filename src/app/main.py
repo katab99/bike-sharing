2899e3cbd45994c61295751c924c415fe4393087
katab99 <bujdosookata@gmail.com>
2026-03-11 21:17:25 +0100

    Add "/" endpoint.

--- src/app/main.py
+++ src/app/main.py
@@ -4,31 +4,32 @@ from fastapi import FastAPI
 import os
 app = FastAPI()
 
-TRACKING_URI = "http://localhost:5000"
 model_name = "Bike-Demand"
-
 
 # load model
 client = MlflowClient()
 model_metadata = client.get_latest_versions(model_name, stages=["None"])
-latest_model = model_metadata[0]
-print(latest_model)
+latest_model = model_metadata[0].source
+model = mlflow.sklearn.load_model(latest_model)
+print(model)
+
 
 
 # def predict(features):
 #     preds = model.predict(features)
 #     return preds
+
+@app.get("/")
+def read_root():
+    return {"message": "Ciao."}
 #
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-#
-# @app.post("/predict")
-# def predict_endpoint(features):
-#     # prepare features
-#
-#     # predict
-#
-#     # format prediction
-#
-#     return
+@app.post("/predict")
+def post_predict(features):
+    # prepare features
+
+    # predict
+    preds = model.fit(features)
+
+    # format prediction
+    print(preds)
+    return preds
